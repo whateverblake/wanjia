8090850f7d220acc7fb07c902e372d6abd7f7d5d
blake <hsb654219812@163.com>
2016-05-07 01:05:24 +0800

    init appserver

---
+++ wanjia-appserver/src/main/java/com/wanjia/wanjia_appserver/App.java
@@ -0,0 +1,13 @@
+package com.wanjia.wanjia_appserver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
